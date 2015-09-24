208e3dc255abd71b69e4ce92f751db766cb1a77f
gitda <gitda@users.noreply.github.com>
2015-09-24 18:41:31 +0700

    Update database.php

--- app/config/database.php
+++ app/config/database.php
@@ -57,7 +57,7 @@ return array(
 			'host'      => 'localhost',
 			'database'  => 'db_inventory',
 			'username'  => 'root',
-			'password'  => '123456test',
+			'password'  => '123456',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
